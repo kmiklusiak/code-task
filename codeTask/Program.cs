96800cefc1d67b8ce79c5730ccfd804afde49fea
kmiklusiak <miklusiakntw@student.agh.edu.pl>
2022-03-30 10:56:15 +0200

    code

---
+++ codeTask/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace codeTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Functions.Check(Functions.A, Functions.B);
+
+            //testing
+            /*            
+            Console.WriteLine("Test:");
+            Test.Testing();
+            */
+        }
+    }
+}
---
+++ codeTask/Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codeTask
+{
+    class Test
+    {
+        public static List<int> A = new List<int> { };
+        public static List<int> B = new List<int> { };
+
+        public static void CreateList(List<int> L)
+        {
+            Random rnd = new Random();
+            int size = 21;
+            for(int i = 0; i < size; i++)
+            {
+                int obj = rnd.Next(0, 10);
+                L.Add(obj);
+            }
+        }
+        public static void Testing()
+        {
+            CreateList(A);
+            CreateList(B);
+            Functions.Check(A, B);
+        }
+    }
+}
